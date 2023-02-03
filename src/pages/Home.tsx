9f1407b84723751ff290d6bd59cd21ef22a8647f
Denis Motichko <motichkodenis10@gmail.com>
2023-02-04 00:13:23 +0200

    added autocomplete and textfield

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
 function Home() {
     return (
         <div>
             <h1>Вітаємо!</h1>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={[]}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="Movie" />}
+            />
         </div>
     )
 }
