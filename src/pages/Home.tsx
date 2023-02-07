33037d265f7df2007108a1bf7483934646fa81be
Denis Motichko <motichkodenis10@gmail.com>
2023-02-07 22:24:53 +0200

    created the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,41 @@
+import React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+import '../styles/Home.scss'
 
 function Home() {
     return (
-        <div>
-            <h1>Вітаємо!</h1>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <div className="container">
+            <Typography variant="h3" gutterBottom sx={{ marginBottom: "20px" }}>
+                Вітаємо!
+            </Typography>
+            <TextField 
+                id="outlined-basic" 
+                label="Введіть ім'я" 
+                variant="outlined" 
+                className="field"
+                sx={{marginBottom: "15px"}}
+            />
             <Autocomplete
                 disablePortal
                 id="combo-box-demo"
                 options={[]}
-                sx={{ width: 300 }}
-                renderInput={(params) => <TextField {...params} label="Movie" />}
+                className="field"
+                renderInput={(params) => <TextField {...params} label="Мрія" />}
             />
+            <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={[]}
+                className="field"
+                renderInput={(params) => <TextField {...params} label="Професія" />}
+            />
+            <Button variant="contained" size="large">
+                Старт
+            </Button>
         </div>
     )
 }
