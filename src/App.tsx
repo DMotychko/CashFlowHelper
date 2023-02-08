5e675cf8287f3a5e02f2908f1169fa92b7187337
Denis Motichko <motichkodenis10@gmail.com>
2023-02-09 01:11:28 +0200

    corrected the errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { appName } from './common/config';
 import MainPage from './pages/MainPage';
+import Home from './pages/Home';
 
 import './styles/App.scss';
 
 function App() {
   return (
     <>
+      <Home/>
       <Helmet>
         <title>{appName}</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,40 +3,45 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Container from '@mui/material/Container';
 
 import '../styles/Home.scss'
 
 function Home() {
     return (
-        <div className="container">
-            <Typography variant="h3" gutterBottom sx={{ marginBottom: "20px" }}>
-                Вітаємо!
-            </Typography>
-            <TextField 
-                id="outlined-basic" 
-                label="Введіть ім'я" 
-                variant="outlined" 
-                className="field"
-                sx={{marginBottom: "15px"}}
-            />
-            <Autocomplete
-                disablePortal
-                id="combo-box-demo"
-                options={[]}
-                className="field"
-                renderInput={(params) => <TextField {...params} label="Мрія" />}
-            />
-            <Autocomplete
-                disablePortal
-                id="combo-box-demo"
-                options={[]}
-                className="field"
-                renderInput={(params) => <TextField {...params} label="Професія" />}
-            />
-            <Button variant="contained" size="large">
-                Старт
-            </Button>
-        </div>
+        <Container fixed className="ch-home-page">
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} className="ch-grid-fields">
+                <Grid item xs={4} sm={6} md={6} >
+                    <Stack spacing={2} className="ch-stack-fields">
+                        <Typography variant="h3" gutterBottom >
+                            Вітаємо!
+                        </Typography>
+                        <TextField 
+                            label="Введіть ім'я" 
+                            variant="outlined" 
+                            className="ch-field"
+                        />
+                        <Autocomplete
+                            disablePortal
+                            options={[]}
+                            className="ch-field"
+                            renderInput={(params) => <TextField {...params} label="Мрія" />}
+                        />
+                        <Autocomplete
+                            disablePortal
+                            options={[]}
+                            className="ch-field"
+                            renderInput={(params) => <TextField {...params} label="Професія" />}
+                        />
+                        <Button variant="contained" size="large">
+                            Старт
+                        </Button>
+                    </Stack>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
 
