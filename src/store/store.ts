256bf1ef44d057de5c334541a0e1e7144a6880ee
Disconaut <omelanchuk98@gmail.com>
2023-02-09 01:16:48 +0200

    Closes #28 - Added action creators for redux dev extension (#29)
    * Added action creators for redux dev extension
    
    * Fixed merge
    
    ---------
    
    Co-authored-by: SOFTSERVE\omela <omela@softserveinc.com>

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,13 @@
 import { combineReducers, configureStore, PreloadedState } from "@reduxjs/toolkit";
-import apartmentsReducer from "./apartmentsSlice";
-import bigBusinessesReducer from "./bigBusinessesSlice";
-import childrenReducer from "./childrenSlice";
-import expensesReducer from "./expensesSlice";
-import isFiredReducer from "./isFiredSlice";
+import apartmentsReducer, { addApartment, removeApartment } from "./apartmentsSlice";
+import bigBusinessesReducer, { addBigBusiness } from "./bigBusinessesSlice";
+import childrenReducer, { addChild } from "./childrenSlice";
+import expensesReducer, { addExpense, removeExpense } from "./expensesSlice";
+import isFiredReducer, { toggleIsFired } from "./isFiredSlice";
 import jobReducer, { setJob } from "./jobSlice";
-import loansReducer from "./loansSlice";
-import smallBusinessesReducer from "./smallBusinessesSlice";
-import userNameReducer from "./userNameSlice";
+import loansReducer, { addLoan, removeLoan } from "./loansSlice";
+import smallBusinessesReducer, { addSmallBusiness, expandSmallBusiness } from "./smallBusinessesSlice";
+import userNameReducer, { setUserName } from "./userNameSlice";
 
 const reducer = combineReducers({
     userName: userNameReducer,
@@ -28,7 +28,19 @@ export const setupStore = (preloadedState?: PreloadedState<StoreState>) => confi
     preloadedState,
     devTools: {
         actionCreators: {
-            setJob
+            setUserName,
+            toggleIsFired,
+            setJob,
+            addChild,
+            addExpense,
+            removeExpense,
+            addLoan,
+            removeLoan,
+            addApartment,
+            removeApartment,
+            addSmallBusiness,
+            expandSmallBusiness,
+            addBigBusiness
         }
     }
 });
