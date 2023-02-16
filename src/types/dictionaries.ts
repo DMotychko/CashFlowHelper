cc65f707d83bb7378756daef8706f3e9294173ac
SOFTSERVE\omela <omela@softserveinc.com>
2023-02-17 01:49:58 +0200

    Created helpers to get dictionaries

---
+++ src/types/dictionaries.ts
@@ -0,0 +1,29 @@
+import type { Job } from ".";
+
+export type JobOption = {
+    id: string;
+    title: string;
+};
+
+export type DreamOption = {
+    title: string;
+    price: number;
+}
+
+export type CardExpense = {
+    title: string;
+    outcome: number;
+}
+
+export type CardLoan = {
+    title: string;
+    sum: number;
+    expense: CardExpense;
+}
+
+export type Card = {
+    job: Job;
+    childExpense: number;
+    expenses: CardExpense[];
+    loans?: CardLoan[];
+}
